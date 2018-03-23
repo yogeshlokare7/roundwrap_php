0ac63529952585548a0963897dd87423ccfef582
Yogesh Bapu Lokare <yogeshlokare7@gmail.com>
2018-03-23 11:56:08 +0530

    Dev: Manasi Des: Added inventorymaster/manage_inventorymaster.php

--- roundwrapnew/inventorymaster/manage_inventorymaster.php
+++ roundwrapnew/inventorymaster/manage_inventorymaster.php
@@ -1,37 +1,48 @@
+<?php
+$listofitems = MysqlConnection::fetchAll("item_master");
+?>
+<title>RoundWrap</title>
+
+<div id="content-header">
+    <div id="breadcrumb"> 
+        <a href="index.php" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a>
+        <a title="View Inventory Master" class="tip-bottom"><i class="icon-home"></i>View Inventory Master</a>
+    </div>
+</div>
 <div class="container-fluid">
-    <br/>
-    dsad
     <div class="widget-box">
         <div class="widget-title">
             <span class="icon"><i class="icon-th"></i></span> 
-            <h5>User Role Table</h5>
+            <h5>Inventory Master</h5>
         </div>
         <div class="widget-content nopadding">
             <table class="table table-bordered data-table">
                 <thead>
                     <tr>
-                        <th>Rendering engine</th>
-                        <th>Browser</th>
-                        <th>Platform(s)</th>
-                        <th>Engine version</th>
+                        <th>#</th>
+                        <th>#</th>                 							
+                        <th>Item Code</th>
+                        <th><Item Description</th>
+                        <th><Item Quantity</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    for ($index = 0; $index < 100; $index++) {
+                    foreach ($listofitems as $key => $value) {
                         ?>
-
                         <tr class="gradeX">
-                            <td>Trident</td>
-                            <td>Internet
-                                Explorer 4.0</td>
-                            <td>Win 95+</td>
-                            <td class="center">4</td>
+                            <td></td>
+                            <td></td>
+                           
+                            <td><?php echo $value["item_code"] ?></td>
+                            <td><?php echo $value["item_desc"] ?></td>
+                             <td><?php echo $value["quantity"] ?></td>
+                            
                         </tr>
-
                         <?php
                     }
                     ?>
+
                 </tbody>
             </table>
         </div>
