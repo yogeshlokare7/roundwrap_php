4a556e15dcdf0871b1043d8ba552be2a3ae6165b
Yogesh Bapu Lokare <yogeshlokare7@gmail.com>
2018-03-23 10:30:53 +0530

    added manage_profilemaster.php

--- roundwrapnew/profilemaster/manage_profilemaster.php
+++ roundwrapnew/profilemaster/manage_profilemaster.php
@@ -1,37 +1,44 @@
+<?php
+$listofprofiles = MysqlConnection::fetchAll("profile_master");
+?>
+<div id="content-header">
+    <div id="breadcrumb"> 
+        <a href="index.php" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a>
+        <a href="index.php" title="Go to Home" class="tip-bottom"><i class="icon-home"></i>View Profile Master</a>
+    </div>
+</div>
 <div class="container-fluid">
-    <br/>
-    dsad
     <div class="widget-box">
         <div class="widget-title">
             <span class="icon"><i class="icon-th"></i></span> 
-            <h5>User Role Table</h5>
+            <h5>Profile Master</h5>
         </div>
         <div class="widget-content nopadding">
             <table class="table table-bordered data-table">
                 <thead>
                     <tr>
-                        <th>Rendering engine</th>
-                        <th>Browser</th>
-                        <th>Platform(s)</th>
-                        <th>Engine version</th>
+                        <th></th>
+                        <th></th>
+                        <th>Profile Name</th>
+                        <th>Label Name</th>
+                        <th>Label Value</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    for ($index = 0; $index < 100; $index++) {
+                    foreach ($listofprofiles as $key => $value) {
                         ?>
-
                         <tr class="gradeX">
-                            <td>Trident</td>
-                            <td>Internet
-                                Explorer 4.0</td>
-                            <td>Win 95+</td>
-                            <td class="center">4</td>
+                            <td></td>
+                            <td></td>
+                            <td><?php echo $value["profile_name"] ?></td>
+                            <td><?php echo $value["label_name"] ?></td>
+                            <td><?php echo $value["label_value"] ?></td>
                         </tr>
-
                         <?php
                     }
                     ?>
+
                 </tbody>
             </table>
         </div>
